9869607f719818ee6ee6217323bb270021d87587
PhilPhronesius <ephrones@msudenver.edu>
2025-11-16 19:13:11 -0700

    Update 2_📊_Charts_Gallery.py

--- streamit_app/pages/2_📊_Charts_Gallery.py
+++ streamit_app/pages/2_📊_Charts_Gallery.py
@@ -32,7 +32,7 @@ df["Color"] = df["Adjusted Rank"].apply(lambda x: "gold" if x <= 10 else "blue")
 college_options = df["College Name"].unique()
 selected_college = st.selectbox("Select College"), options = ["All Colleges"] + list(college_options), index = 0)
 
-if selected_college != "All Colleges"]:
+if selected_college != "All Colleges":
     filtered_df = df[df["College Name"] == selected_college]
 else:
     filtered_df = df
